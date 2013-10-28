c9868bd7c2cd76ab5767a8f1cdfa26de4fbce3a4
erishabh@gmail.com <erishabh@gmail.com@77110760-8bcc-3978-7cb7-f881e6430454>
2013-10-28 03:06:08 +0000

    committing changes in the preproessing/ directory

--- trunk/src/preprocessing/Dictionary.java
+++ trunk/src/preprocessing/Dictionary.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 
 import datastr.*;
@@ -22,6 +24,7 @@ public class Dictionary {
 		int len=dataset.Tweets.size();
 		Iterator<Tweet> itr=dataset.Tweets.iterator();
 		int tempidf=0;int twno=0;
+		
 		while(itr.hasNext())
 		{
 			//now deal with each tweet and insert the words in each tweet in Dictionary if its not present already
@@ -31,6 +34,7 @@ public class Dictionary {
 			Iterator<Word> wrditr=list.iterator();
 			Word tempw;tempidf=0;
 			//for each word in this particular tweet modify the dictionary & wcl.list accordingly
+			
 			while(wrditr.hasNext())
 			{
 				Word w=wrditr.next();
@@ -38,16 +42,24 @@ public class Dictionary {
 				w.word=w.word.toLowerCase();
 				if(w.word.length()>1)
 				{	
+					//if(w.word.equals("equipment")) System.out.println("outside");
 					//---------------------------------------------insert in Dictionary start---------------------------------------
 					if(dataset.dictionary.containsKey(w.word)) 
 					{
+						//if(w.word.equals("equipment")) System.out.println("inside yes");
 						//if(w.word.equals("brave")) System.out.print("_Dictionary Contains_");
 						tempw=dataset.dictionary.get(w.word);
 						tempw.count+=w.count;
+						//tempw.count++;
+						//if(w.word.equals("equipment")) System.out.println("inside yes-count increased");
+						
 						tempw.idf++;
+						
+						dataset.dictionary.put(tempw.word, tempw);//added this on 6th June 2012
 					}
 					else
 					{
+						//if(w.word.equals("equipment")) System.out.println("inside else");
 						dataset.dictionary.put(w.word, w);
 					}
 					//---------------------------------------------insert in Dictionary end---------------------------------------
@@ -71,6 +83,7 @@ public class Dictionary {
 							cpd=new CountPerDay(tw.getDate(),1,wc.word,wc.total_days);
 							wc.dayList.add(cpd);
 						}
+						wcl.list.put(wc.word.word, wc);
 					}
 					else
 					{
@@ -96,15 +109,84 @@ public class Dictionary {
 			BufferedWriter out = new BufferedWriter(fstream);
 			Collection<Word> c=dataset.dictionary.values();
 			Iterator<Word> itr=c.iterator();
+			ArrayList<Word> forSort=new ArrayList<Word>();
 			while(itr.hasNext())
 			{
 				Word w=itr.next();
-				if(w.idf>10)out.write(w.word+" idf value= "+w.idf+" total count = "+w.count+"\n");
+				forSort.add(w);
+				//if(w.idf>10)
+					out.write(w.word+" idf value= "+w.idf+" total count = "+w.count+"\n");
 			}
+			out.close();
+			printSortByCount(forSort);
+			printSortByIdf(forSort);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
+	public void printSortByCount(ArrayList<Word> forSort)
+	{
+		Collections.sort(forSort, new Comparator<Object>(){
+	        public int compare(Object o1, Object o2) {
+            Word p1 = (Word) o1;
+            Word p2 = (Word) o2;
+            /*if(p1.burstScore==p2.burstScore) return 0;
+            else if(p1.burstScore<p2.burstScore) return 1;*/
+            if(p1.count==p2.count) return 0;
+            else if(p1.count<p2.count) return 1;
+            else return -1;
+	        }
+		});
+		Iterator<Word> itr=forSort.iterator();
+		try {
+			FileWriter fstream;
+			
+			fstream = new FileWriter("results/final/sortByCount"+thread);
+			BufferedWriter out = new BufferedWriter(fstream);
+			while(itr.hasNext())
+			{
+				Word w=itr.next();
+			
+				out.write("Word= "+w.word+"\tCount= "+w.count+"\tidf= "+w.idf+"\n");
+			
+			}
+			out.close();
+		}catch (IOException e) {e.printStackTrace();}
+	}
+	
+	public void printSortByIdf(ArrayList<Word> forSort)
+	{
+		Collections.sort(forSort, new Comparator<Object>(){
+	        public int compare(Object o1, Object o2) {
+            Word p1 = (Word) o1;
+            Word p2 = (Word) o2;
+            /*if(p1.burstScore==p2.burstScore) return 0;
+            else if(p1.burstScore<p2.burstScore) return 1;*/
+            if(p1.idf==p2.idf) return 0;
+            else if(p1.idf<p2.idf) return 1;
+            else return -1;
+	        }
+		});
+		
+		Iterator<Word> itr=forSort.iterator();
+		
+		FileWriter fstream;
+		
+		try {
+			fstream = new FileWriter("results/final/sortByIdf"+thread);
+			BufferedWriter out = new BufferedWriter(fstream);
+			
+			while(itr.hasNext())
+			{
+				Word w=itr.next();
+				
+				out.write("Word= "+w.word+"\tIDF= "+w.idf+"\tCount= "+w.count+"\n");
+			
+			}
+			}catch (IOException e) {e.printStackTrace();}
+	}
+	
 }
+
